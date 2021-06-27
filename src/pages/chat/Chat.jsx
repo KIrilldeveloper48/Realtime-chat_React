fff02c10e5d5d60718599c81d8f45c1114e57c78
Kirill <developerpopov@yandex.ru>
2021-06-27 17:50:18 +0300

    Добавил функционал для загрузки и отправки сообщений

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
 
+import firebase from 'firebase/app';
+
+import Loader from '@components/loader/Loader';
+
+import {Context} from '../..';
 import './chat.less';
 
 const Chat = () => {
-  return (<div>Chat</div>);
+  const {auth, firestore} = useContext(Context);
+  const [userData] = useAuthState(auth);
+  const [message, setMessage] = useState(``);
+  const [messages, loading] = useCollectionData(
+      firestore.collection(`messages`).orderBy(`createdAt`)
+  );
+
+  const textfieldChangeHandler = (evt) => {
+    setMessage(evt.target.value);
+  };
+
+  const submitMessage = async () => {
+    firestore.collection(`messages`).add({
+      userId: userData.uid,
+      userName: userData.displayName,
+      userAvatar: userData.photoURL,
+      userMessage: message,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    });
+    setMessage(``);
+  };
+
+  return (
+    loading ? <Loader />
+      :
+      <section className="Chat">
+        <div className="chat__window">
+          {messages.map((messageData) => {
+            return (
+              <div>
+                <img src={messageData.userAvatar} alt="" />
+                <p>{messageData.userName}</p>
+                <p>{messageData.userMessage}</p>
+              </div>
+            );
+          })}
+
+        </div>
+        <input type="textarea" value={message} onChange={textfieldChangeHandler}/>
+        <button onClick={submitMessage}>Ок</button>
+      </section>
+  );
 };
 
 export default Chat;
