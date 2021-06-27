d05b6c58f745e1598cf3a3f9543424b3a3a98dcc
Kirill <developerpopov@yandex.ru>
2021-06-27 11:47:55 +0300

    Выделил несколько основных компонент

---
+++ src/components/message/Message.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './message.less';
+
+const Message = () => {
+  return (<div>Message</div>);
+};
+
+export default Message;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './navbar.less';
+
+const Navbar = () => {
+  return (
+    <p>navbar</p>
+  );
+};
+
+export default Navbar;
