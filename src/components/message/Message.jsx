6c9a67dea09d5ec06854e8713925af7e2cfb03de
Kirill <developerpopov@yandex.ru>
2021-06-27 22:16:46 +0300

    Сделал и застилизовал компонент для сообщения

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import {PropTypes} from 'prop-types';
+
 
 import './message.less';
 
-const Message = () => {
-  return (<div>Message</div>);
+const Message = ({avatar, name, message}) => {
+  return (
+
+    <article className="message">
+      <img className="message__user-avatar" src={avatar} alt="" />
+      <span className="message__user-name">{name}</span>
+      <p className="message__text">{message}</p>
+    </article>
+
+  );
 };
 
-export default Message;
+Message.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+};
+
+export default React.memo(Message);
