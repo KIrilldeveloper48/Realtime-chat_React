7f825c31cfbc9ecd44475085c330048f7aa2be8e
Kirill <developerpopov@yandex.ru>
2021-06-27 17:49:50 +0300

    Добавил отрисовку лоадера на время загрузки данных

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import React, {useContext} from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
+import Navbar from '@components/navbar/Navbar';
 import Login from '../../pages/login/Login';
 import Chat from '../../pages/chat/Chat';
 import Main from '../../pages/main/Main';
 import Page404 from '../../pages/page-404/Page-404';
 import PrivateRoute from '../hocs/private-route/Private-route';
+import Loader from '@components/loader/Loader';
 
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {Context} from '../..';
@@ -17,35 +19,45 @@ import './app.less';
 
 const App = () => {
   const {auth} = useContext(Context);
-  const [userData] = useAuthState(auth);
+  const [userData, loadingStatus] = useAuthState(auth);
   const isLogined = Boolean(userData);
 
   return (
     <BrowserRouter>
+      <Navbar />
+      <section className="main-content">
+        {
+          loadingStatus ?
 
-      <Switch>
+            <Loader />
+            :
+            <Switch>
 
-        <Route exact path={Routes.MAIN}>
-          <Main />
-        </Route>
+              <Route exact path={Routes.MAIN}>
+                <Main />
+              </Route>
 
-        <PrivateRoute exact
-          path={Routes.LOGIN}
-          render={() => <Login />}
-          isLogined={isLogined}>
-        </PrivateRoute>
+              <PrivateRoute exact
+                path={Routes.LOGIN}
+                render={() => <Login />}
+                isLogined={isLogined}>
+              </PrivateRoute>
 
-        <PrivateRoute exact
-          path={Routes.CHAT}
-          render={() => <Chat />}
-          isLogined={isLogined}>
-        </PrivateRoute>
+              <PrivateRoute exact
+                path={Routes.CHAT}
+                render={() => <Chat />}
+                isLogined={isLogined}>
+              </PrivateRoute>
 
-        <Route>
-          <Page404 />
-        </Route>
+              <Route>
+                <Page404 />
+              </Route>
 
-      </Switch>
+            </Switch>
+        }
+
+      </section>
+
     </BrowserRouter>
   );
 };
