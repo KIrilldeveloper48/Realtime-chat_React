00bdb6d90c1b67465e1de323b208c3b878b13088
Kirill <developerpopov@yandex.ru>
2021-06-27 15:17:19 +0300

    Подключил  и проинициализировал firebase

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,34 @@
 import 'normalize.css';
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom';
 import App from '@components/App/App';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
+firebase.initializeApp(
+    {
+      apiKey: `AIzaSyC-lDQtlA3zFgCGb9iQwBRsNsuYADsKfgE`,
+      authDomain: `realtime-chat-af52c.firebaseapp.com`,
+      projectId: `realtime-chat-af52c`,
+      storageBucket: `realtime-chat-af52c.appspot.com`,
+      messagingSenderId: `353426360424`,
+      appId: `1:353426360424:web:899b0aac2084a92bc4d3dd`
+    }
+);
+
+export const Context = createContext(null);
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 ReactDOM.render(
-    <App />,
+    <Context.Provider value={{
+      firebase,
+      auth,
+      firestore
+    }}>
+      <App />
+    </Context.Provider>,
     document.querySelector(`#root`)
 );
