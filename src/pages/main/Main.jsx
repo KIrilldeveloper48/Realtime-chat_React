c837e95d5c4b67bb34548db3d5113abd83dc0538
Kirill <developerpopov@yandex.ru>
2021-06-27 22:14:07 +0300

    Сделал стилизацию для главной

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import {Routes} from '../../consts';
 
+import './main.less';
+
 const Main = () => {
 
   return (
-    <div>
-      <p>Realtime chat - это инновационная платформа для общения пользователей</p>
-      <NavLink to={Routes.CHAT}>Начать</NavLink>
+    <div className="first-screen">
+      <h1 className="first-screen__title">Realtime chat <br /> <span>инновационная платформа для общения</span></h1>
+      <NavLink className="first-screen__start btn" to={Routes.CHAT}><span>Поехали</span></NavLink>
     </div>
   );
 };
