603cfbb0f7f1a94e448e217bc8880372ca09a29a
Kirill <developerpopov@yandex.ru>
2021-06-28 21:22:14 +0300

    Оформил страницу 404

--- src/pages/page-404/Page-404.jsx
+++ src/pages/page-404/Page-404.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import {NavLink} from 'react-router-dom';
+import {Routes} from '../../consts';
 import './page-404.less';
 
+
+/**
+ * Страница 404
+ * @component
+ * @return {func} Возвращает cтраницу 404
+ */
 const Page404 = () => {
-  return (<div>Упс</div>);
+  return (
+    <div className="page-404">
+      <h1 className="page-404__title">Упс, У нас нет такой страницы</h1>
+      <NavLink to={Routes.MAIN}><button className="btn"><span>Вернуться</span></button></NavLink>
+    </div>
+  );
 };
 
 export default Page404;
