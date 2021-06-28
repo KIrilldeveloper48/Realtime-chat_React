4cea4912d4a88f535ce00b2d1fcd5110759f3206
Kirill <developerpopov@yandex.ru>
2021-06-28 21:12:07 +0300

    Добавил блокировку условие для блокировки кнопки входа, задокументировал

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,23 +1,44 @@
-import React, {useContext} from 'react';
-import firebase from 'firebase/app';
+import React, {useContext, useState} from 'react';
 
 import {Context} from '../..';
+import {authorization} from '../../api';
+
 import './login.less';
 
-
+/**
+ * Страница логина
+ * @component
+ * @return {func} Возвращает страницу логина
+ */
 const Login = () => {
+
+  /**
+   * Достаём обьект firebase для авторизации пользователя из контекста
+   * @type {Object}
+   * @property {Object} auth1
+   */
   const {auth} = useContext(Context);
 
-  const loginClickHandler = async () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    // eslint-disable-next-line no-unused-vars
-    const {user} = await auth.signInWithPopup(provider);
+  /**
+   * Будем хранить состоянии кнопки "Войти". Когда открыт popup входа, кнопка будет заблокирована
+   * @type {Array<bool, func>}
+   */
+  const [disabledStatus, setDisabledStatus] = useState(false);
+
+  /**
+   * Обработчик клика для кнопки "Войти" для авторизации через Google
+   * @type {func}
+   * @listens Click
+   */
+  const loginClickHandler = () => {
+    setDisabledStatus(true);
+    authorization(auth).catch(() => setDisabledStatus(false));
   };
 
   return (
     <>
       <div className="login">
-        <button className="login__btn btn" onClick={loginClickHandler}><span>Войти через Google</span></button>
+        <button className="login__btn btn" disabled={disabledStatus} onClick={loginClickHandler}><span>Войти через Google</span></button>
       </div>
     </>
   );
